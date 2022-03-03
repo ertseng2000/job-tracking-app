727055232a022cc1b6b41debc4c0ecf8500d4020
wesleyyin <wesleyyin@wustl.edu>
2022-03-03 17:23:43 -0600

    added edit and delete event, fixed date stuff

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -27,10 +27,14 @@ export default function Timeline() {
   const [pageReady, setReady] = useState(false);
   const [events, setEvents] = useState([{
     tag: '',
-    date: ''
+    date: new Date(),
+    id: ''
   }]);
   const [tag, setTag] = useState('Applied');
   const [date, setDate] = useState('');
+
+  const [editTag, setEditTag] = useState('Applied');
+  const [editDate, setEditDate] = useState('');
 
 
   // Firebase auth observer. Sends user back to login page if not signed in
@@ -54,7 +58,8 @@ export default function Timeline() {
       
       setEvents(querySnapshot.docs.map(doc => ({
         tag: doc.data().tag,
-        date: doc.data().date.toDate()
+        date: new Date(doc.data().date.toDate()),
+        id: doc.id
       })));
     });
   }
@@ -95,6 +100,10 @@ export default function Timeline() {
   const submitEvent = async () => {
     console.log(tag);
     const asDate = new Date(date);
+
+    //fix off-by-one error when converting from jsx to js date
+    asDate.setDate(asDate.getDate() + 1);
+
     console.log(asDate);
     
     const updatesPath = 'updates/' + auth.currentUser.uid + '/appUpdates';
@@ -105,6 +114,30 @@ export default function Timeline() {
       companyName: companyName
     });
     
+  };
+
+  const submitEditEvent = async (id) => {
+    console.log(editTag);
+    const asDate = new Date(editDate);
+    asDate.setDate(asDate.getDate() + 1);
+    console.log(asDate);
+    
+    const updatesPath = 'updates/' + auth.currentUser.uid + '/appUpdates';
+    await setDoc(doc(collection(db, updatesPath), id), {
+      tag: editTag,
+      date: asDate,
+      appId: applicationId,
+      companyName: companyName
+    });
+    
+  };
+
+  //Deletes current application and redirects to application page
+  const deleteEvent = async(id) => {
+    //First delete all updates corresponding to the current app
+    const updatesPath = 'updates/' + auth.currentUser.uid + '/appUpdates';
+    await deleteDoc(doc(db, updatesPath, id));
+
   };
 
   // Renders page after loading
@@ -123,7 +156,7 @@ export default function Timeline() {
           <button onClick={calendar}>Calendar</button>
         </nav>
         <button onClick={deleteApp}>Delete This Application</button>
-        <Popup trigger={<button>Add New Event</button>} position="right center">
+        <Popup trigger={<button>New Update</button>} position="right center">
           <div>Fill in this form!</div>
           <label for="tags">Select an Event Type:</label>
           <select id="tags" name="tags" onChange={(e) => {setTag(e.target.value)}} >
@@ -148,7 +181,30 @@ export default function Timeline() {
         {events.map((event) =>
             <div className = "timeLineSection"> 
             <h1>{event.tag}</h1>
-            <p>{event.date.toString()}</p>
+            <p>{event.date.toDateString()}</p>
+
+            {/* Pop up form for edit event */}
+            <Popup trigger={<button>EDIT</button>} position="right center">
+              <div>Fill in this form!</div>
+              <label for="tags">Select an Event Type:</label>
+              <select id="tags" name="tags" onChange={(e) => {setEditTag(e.target.value)}} >
+                <option value="Applied">Applied</option>
+                <option value="Take Home Assessment">Take Home Assessment</option>
+                <option value="Phone Screen">Phone Screen</option>
+                <option value="Interview">Interview</option>
+                <option value="Technical Interview">Technical Interview</option>
+                <option value="Final Round Interview">Final Round Interview</option>
+                <option value="Offer">Offer</option>
+                <option value="Rejected">Rejected</option>
+                
+                
+              </select>
+              <input type='date' onChange={(e) => {setEditDate(e.target.value)}} />
+              
+
+              <button id='submitButton' onClick={() => submitEditEvent(event.id)}>Submit</button>
+            </Popup>
+            <button onClick={() => deleteEvent(event.id)}>DELETE</button>
             </div>
           )}
       </>
